5eff21100f3587f723a133fdad5394aa4eb2cb11
Kevin Bett <prodigy4614@gmail.com>
2024-10-06 10:35:35 +0300

    update dodger left and right motion

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const dodger = document.getElementById("dodger");
 
+// moves the dodger left
 function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
@@ -8,9 +9,21 @@ function moveDodgerLeft() {
       dodger.style.left = `${left - 1}px`;
     }
   }
+//moves the dodger right
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
   
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+ 
   document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowLeft") {
       moveDodgerLeft();
+    }else 
+    if (e.key === "ArrowRight"){
+        moveDodgerRight();
     }
   });
